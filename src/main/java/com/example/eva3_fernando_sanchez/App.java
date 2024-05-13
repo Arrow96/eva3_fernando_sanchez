a6f47dc60338027a0f3849e640b71fe6763aefb2
Arrow961 <nanicio96@gmail.com>
2024-05-13 14:24:49 -0300

    init

---
+++ src/main/java/com/example/eva3_fernando_sanchez/App.java
@@ -0,0 +1,13 @@
+package com.example.eva3_fernando_sanchez;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Evaluacion N°3 Integracion Continua - Fernando Alejandro Sanchez Gallardo!" );
+    }
+}
